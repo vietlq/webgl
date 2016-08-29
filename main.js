66410fcb7f442009f718cb92cf3a48696a6048ff
Viet Le <vietlq85@gmail.com>
2016-08-29 11:41:06 +0100

    Basic GLSL demo

---
+++ main.js
@@ -0,0 +1,61 @@
+// Vertices & Shaders (GLSL)
+// https://material.google.com/style/color.html#color-color-palette
+
+var gl, shaderProgram;
+
+initGL();
+createShaders();
+createVertices();
+draw();
+
+function initGL() {
+    var canvas = document.getElementById("canvas");
+    gl = canvas.getContext("webgl");
+    //gl = canvas.getContext("experimental-webgl");
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    // RGBA (0 -> 1)
+    gl.clearColor(0.1, 0.6, 0.9, 1);
+}
+
+function createShaders() {
+    // Vertex Shader Source
+    var vs = "";
+    vs += "attribute vec4 coords;"
+    vs += "void main(void) {";
+    vs += "    gl_Position = coords;";
+    vs += "    gl_PointSize = 100.0;";
+    vs += "}";
+    // Compile Vertex Shader
+    var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertexShader, vs);
+    gl.compileShader(vertexShader);
+
+    // Fragment Shader Source
+    var fs = "";
+    fs += "void main(void) {";
+    fs += "    gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);";
+    fs += "}";
+    // Compile Fragment Shader
+    var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fragmentShader, fs);
+    gl.compileShader(fragmentShader);
+
+    // Create Shader Program
+    shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
+    gl.useProgram(shaderProgram);
+}
+
+function createVertices() {
+    var coords = gl.getAttribLocation(shaderProgram, "coords");
+    gl.vertexAttrib3f(coords, 0, .5, 0);
+}
+
+function draw() {
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    // Offset, number of vertices
+    gl.drawArrays(gl.POINTS, 0, 1);
+}
